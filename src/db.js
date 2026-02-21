08bcb6cbd1ae241addc4edba056d9b3d3fd8aec8
f0620123-png <f0620123@gmail.com>
2026-02-21 16:03:00 +0800

    Update db.js

--- src/db.js
+++ src/db.js
@@ -2,6 +2,7 @@
 const DB_NAME = "wardrobe_db";
 const STORE_NAME = "images";
 
+// 開啟或建立 IndexedDB 資料庫
 export function openDB() {
   return new Promise((resolve, reject) => {
     const req = indexedDB.open(DB_NAME, 1);
@@ -13,6 +14,7 @@ export function openDB() {
   });
 }
 
+// 儲存大圖 (原圖)
 export async function saveFullImage(id, base64) {
   const db = await openDB();
   return new Promise((resolve, reject) => {
@@ -23,6 +25,7 @@ export async function saveFullImage(id, base64) {
   });
 }
 
+// 讀取大圖 (原圖)
 export async function loadFullImage(id) {
   const db = await openDB();
   return new Promise((resolve, reject) => {
@@ -33,45 +36,12 @@ export async function loadFullImage(id) {
   });
 }
 
+// 刪除大圖 (刪除衣物時呼叫)
 export async function deleteFullImage(id) {
   const db = await openDB();
   return new Promise((resolve, reject) => {
     const tx = db.transaction(STORE_NAME, "readwrite");
     tx.objectStore(STORE_NAME).delete(id);
-    tx.oncomplete = () => resolve();
-    tx.onerror = () => reject(tx.error);
-  });
-}
-
-// ✨ [v15.5 新增] 匯出所有大圖
-export async function getAllImages() {
-  const db = await openDB();
-  return new Promise((resolve, reject) => {
-    const tx = db.transaction(STORE_NAME, "readonly");
-    const store = tx.objectStore(STORE_NAME);
-    const reqKeys = store.getAllKeys();
-    const reqVals = store.getAll();
-    
-    tx.oncomplete = () => {
-      const keys = reqKeys.result;
-      const vals = reqVals.result;
-      const res = {};
-      keys.forEach((k, i) => { res[k] = vals[i]; });
-      resolve(res);
-    };
-    tx.onerror = () => reject(tx.error);
-  });
-}
-
-// ✨ [v15.5 新增] 匯入並覆寫大圖
-export async function putAllImages(dataObj) {
-  const db = await openDB();
-  return new Promise((resolve, reject) => {
-    const tx = db.transaction(STORE_NAME, "readwrite");
-    const store = tx.objectStore(STORE_NAME);
-    for (const [id, base64] of Object.entries(dataObj)) {
-      store.put(base64, id);
-    }
     tx.oncomplete = () => resolve();
     tx.onerror = () => reject(tx.error);
   });

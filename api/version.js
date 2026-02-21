f9242e4f714bd68246d8ee20bdaa0a907be41047
f0620123-png <f0620123@gmail.com>
2026-02-21 15:02:49 +0800

    Update version.js

--- api/version.js
+++ api/version.js
@@ -2,8 +2,8 @@ export default function handler(req, res) {
   res.setHeader("Cache-Control", "no-store, max-age=0");
 
   res.status(200).json({
-    appVersion: "v15.2",
-    appSemver: "15.2.0",
+    appVersion: "v15.5",
+    appSemver: "15.5.0",
     vercelEnv: process.env.VERCEL_ENV || "local",
     deploymentId: process.env.VERCEL_DEPLOYMENT_ID || "local",
     git: {
